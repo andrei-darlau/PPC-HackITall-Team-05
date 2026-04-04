1e025cfe41b2b6965d17443e2323cf17b51952a9
Andrei Darlau <andreidarlau04@gmail.com>
2026-04-04 11:33:18 +0300

    default spring proj

---
+++ backend_api/src/main/java/eu/urzicroft/turbine/rest/TestRestController.java
@@ -0,0 +1,14 @@
+package eu.urzicroft.turbine.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ backend_api/src/test/java/eu/urzicroft/turbine/TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.urzicroft.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
