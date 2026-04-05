6acd4c04ab04a938ea8334c29123340630e51f73
Mihnea Gheorghe <mihnea.gheorghe16@gmail.com>
2026-04-05 06:53:51 +0300

    TSO Report

--- backend_api/src/main/java/eu/urzicroft/turbine/controller/ReportController.java
+++ backend_api/src/main/java/eu/urzicroft/turbine/controller/ReportController.java
@@ -2,20 +2,36 @@ package eu.urzicroft.turbine.controller;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/api/v1/reports")
 public class ReportController {
 
-    @GetMapping("/download/{fileName}")
-    public ResponseEntity<Resource> downloadReport(@PathVariable String fileName) {
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/tso")
+    public ResponseEntity<Resource> downloadTsoReport(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+        if (date == null) {
+            date = LocalDate.now();
+        }
+
+        String fileName = "TSO_Report_" + date.format(DateTimeFormatter.ISO_DATE) + ".csv";
         File file = new File(fileName);
-        if (!file.exists()) return ResponseEntity.notFound().build();
+
+        if (!file.exists()) {
+            return ResponseEntity.notFound().build();
+        }
 
         Resource resource = new FileSystemResource(file);
         return ResponseEntity.ok()
